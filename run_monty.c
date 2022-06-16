af0e8e7c5bd4b8cdcbc89e73a51df64ee6497fb5
otero nick <99287159+O-tero@users.noreply.github.com>
2022-06-17 00:58:07 +0300

    run_monty.c

--- run_monty.c
+++ run_monty.c
@@ -8,7 +8,7 @@ void (*get_op_func(char *opcode))(stack_t**, unsigned int);
 int run_monty(FILE *script_fd);
 
 /**
- * free_tokens - Frees the global op_toks array of strings
+ * free_tokens - Frees the global op_toks array of strings.
  */
 void free_tokens(void)
 {
@@ -43,7 +43,7 @@ unsigned int token_arr_len(void)
  * @delims: A string of delimiter characters.
  *
  * Return: If the line only contains delimiters - 1.
- *        Otherwise - 0.
+ *         Otherwise - 0.
  */
 int is_empty_line(char *line, char *delims)
 {
@@ -55,9 +55,9 @@ int is_empty_line(char *line, char *delims)
 		{
 			if (line[i] == delims[j])
 				break;
-		}										
+		}
 		if (delims[j] == '\0')
-		return (0);
+			return (0);
 	}
 
 	return (1);
@@ -88,14 +88,15 @@ void (*get_op_func(char *opcode))(stack_t**, unsigned int)
 		{"rotl", monty_rotl},
 		{"rotr", monty_rotr},
 		{"stack", monty_stack},
-		{"queue", monty_queue},																		{NULL, NULL}
+		{"queue", monty_queue},
+		{NULL, NULL}
 	};
 	int i;
 
 	for (i = 0; op_funcs[i].opcode; i++)
 	{
 		if (strcmp(opcode, op_funcs[i].opcode) == 0)
-			return (op_funcs[i].f);										
+			return (op_funcs[i].f);
 	}
 
 	return (NULL);
@@ -114,54 +115,55 @@ int run_monty(FILE *script_fd)
 	size_t len = 0, exit_status = EXIT_SUCCESS;
 	unsigned int line_number = 0, prev_tok_len = 0;
 	void (*op_func)(stack_t**, unsigned int);
+
 	if (init_stack(&stack) == EXIT_FAILURE)
 		return (EXIT_FAILURE);
 
 	while (getline(&line, &len, script_fd) != -1)
 	{
 		line_number++;
-		op_toks = strtow(line, DELIMS);																	if (op_toks == NULL)
-																				{
-																								if (is_empty_line(line, DELIMS))
-																									continue;
-																								free_stack(&stack);
-																								return (malloc_error());
-																				}
-		else if (op_toks[0][0] == '#') /* comment line */
+		op_toks = strtow(line, DELIMS);
+		if (op_toks == NULL)
 		{
-																										free_tokens();
-																										continue;
-																										;
-																					op_func = get_op_func(op_toks[0]);
-																					if (op_func == NULL)
-																					{
-																						free_stack(&stack);
-																						exit_status = unknown_op_error(op_toks[0], line_number);
-																						free_tokens();
-																						break;
-																					}
-																					prev_tok_len = token_arr_len();
-																					op_func(&stack, line_number);
-																					if (token_arr_len() != prev_tok_len)
-																					{
-																						if (op_toks && op_toks[prev_tok_len])
-																																									exit_status = atoi(op_toks[prev_tok_len]);																																							else
-																																										exit_status = EXIT_FAILURE;
-																						free_tokens();
-																																										break;
-																					}
-																					free_tokens();
-
-		}									
-		free_stack(&stack);
-
-
-		if (line && *line == 0)
-		{
-			free(line);
+			if (is_empty_line(line, DELIMS))
+				continue;
+			free_stack(&stack);
 			return (malloc_error());
 		}
+		else if (op_toks[0][0] == '#') /* comment line */
+		{
+			free_tokens();
+			continue;
+		}
+		op_func = get_op_func(op_toks[0]);
+		if (op_func == NULL)
+		{
+			free_stack(&stack);
+			exit_status = unknown_op_error(op_toks[0], line_number);
+			free_tokens();
+			break;
+		}
+		prev_tok_len = token_arr_len();
+		op_func(&stack, line_number);
+		if (token_arr_len() != prev_tok_len)
+		{
+			if (op_toks && op_toks[prev_tok_len])
+				exit_status = atoi(op_toks[prev_tok_len]);
+			else
+				exit_status = EXIT_FAILURE;
+			free_tokens();
+			break;
+		}
+		free_tokens();
+	}
+	free_stack(&stack);
 
+	if (line && *line == 0)
+	{
 		free(line);
-		return (exit_status);
+		return (malloc_error());
+	}
+
+	free(line);
+	return (exit_status);
 }
