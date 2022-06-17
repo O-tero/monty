772c24fa0909916ed4d036f0d7aa8099dae3d3bb
otero nick <99287159+O-tero@users.noreply.github.com>
2022-06-17 11:29:30 +0300

    Stacks, Queues - LIFO, FIFO

--- stack.c
+++ stack.c
@@ -1,23 +1,15 @@
 #include "monty.h"
 
-
-
 /**
+ * _stack - select stack mode (default).
+ * @stack: double pointer to header (top) of the stack.
+ * @line_number: counter for line number of the file.
  *
- *  * _stack - select stack mode (default).
- *
- *   * @stack: double pointer to header (top) of the stack.
- *
- *    * @line_number: counter for line number of the file.
- *
- *     *
- *
- *      * Return: void.
- *
- *       */
+ * Return: void.
+ */
 void _stack(stack_t **stack, unsigned int line_number)
 {
-		(void) stack;
-			(void) line_number;
-				stack_queue = 's';
+	(void) stack;
+	(void) line_number;
+	stack_queue = 's';
 }
