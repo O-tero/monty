6616c170cf1b43775c2b809bb8d8e33f1499e8ae
O-tero <oteronickbrian@gmail.com>
2022-06-17 03:12:27 -0700

    Stacks, Queues - LIFO, FIFO

--- add.c
+++ add.c
@@ -1,27 +1,167 @@
 #include "monty.h"
 
+
+
 /**
- * _add - adds the top two elements of the stack.
- * @stack: double pointer to header (top) of the stack.
- * @line_number: counter for line number of the file.
- * 
- * Return: void.
- */
-void _add(stack_t **stack, unsigned int line_number)
+ *
+ *  * check_add - check if a string is pint
+ *
+ *   * @str: string
+ *
+ *    *
+ *
+ *     * Return: 1 true or 0 false
+ *
+ *      */
+
+int check_add(char *str)
+
 {
-	stack_t *temp;
 
-	if (!stack || !*stack || !((*stack)->next))
-	{
-		fprintf(stderr, "L%d: can't add, stack too short\n", line_number);
-		free_stack_t(*stack);
+		char *a = NULL;
 
-		exit(EXIT_FAILURE);
-	}
 
-	temp = *stack;
-	*stack = (*stack)->next;
-	(*stack)->n += temp->n;
-	(*stack)->prev = NULL;
-	free(temp);
+
+			if (str[0] != 'a')
+
+						return (0);
+
+				if (str[1] != 'd')
+
+							return (0);
+
+					if (str[2] != 'd')
+
+								return (0);
+
+						a = &str[3];
+
+							while (*a != '\0' && *a != '\n')
+
+									{
+
+												if (*a != ' ')
+
+																return (0);
+
+														a++;
+
+															}
+
+								return (1);
+
+}
+
+
+
+/**
+ *
+ *  * check_nop - check if a string is pint
+ *
+ *   * @str: string
+ *
+ *    *
+ *
+ *     * Return: 1 true or 0 false
+ *
+ *      */
+
+int check_nop(char *str)
+
+{
+
+		char *a = NULL;
+
+
+
+			if (str[0] != 'n')
+
+						return (0);
+
+				if (str[1] != 'o')
+
+							return (0);
+
+					if (str[2] != 'p')
+
+								return (0);
+
+						a = &str[3];
+
+							while (*a != '\0' && *a != '\n')
+
+									{
+
+												if (*a != ' ')
+
+																return (0);
+
+														a++;
+
+															}
+
+								return (1);
+
+}
+
+
+
+/**
+ *
+ *  * add - prints the top of a stack
+ *
+ *   * @stack: head of stack
+ *
+ *    * @ln: line number
+ *
+ *     *
+ *
+ *      * Return: void
+ *
+ *       */
+
+void add(stack_t **stack, unsigned int ln)
+
+{
+
+		stack_t *h, *cnt;
+
+
+
+			if (stack == NULL || *stack == NULL)
+
+					{
+
+								fprintf(stderr,
+
+													"L%d: can't add, stack too short\n", ln);
+
+										exit(EXIT_FAILURE);
+
+											}
+
+				h = *stack;
+
+					cnt = h->next;
+
+						if (cnt == NULL)
+
+								{
+
+											fprintf(stderr,
+
+																"L%d: can't add, stack too short\n", ln);
+
+													free_stack(*stack);
+
+															exit(EXIT_FAILURE);
+
+																}
+
+							cnt->prev = NULL;
+
+								cnt->n += h->n;
+
+									*stack = cnt;
+										free(h);
 }
